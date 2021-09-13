dcd586175ff9b9d394bc7fe6f36963a90458146c
David Gauntlett <david.m.gauntlett@gmail.com>
2021-09-13 00:36:55 -0400

    added images to gallery page

--- src/views/PhotoGallery.vue
+++ src/views/PhotoGallery.vue
@@ -1,15 +1,44 @@
 <template>
-  <div class="gallery">
-    photos here
+  <div class="d-flex flex-wrap">
+    <b-img
+      v-for="picture in images"
+      :key="picture.id"
+      :src="`http://localhost:1337${picture.url}`"
+      class="m-2 w-25"
+    />
   </div>
 </template>
 
 <script>
+  import axios from 'axios';
   export default {
-    name: "PhotoGallery"
+    name: "PhotoGallery",
+
+    data(){
+      return {
+        carts: []
+      }
+    },
+
+    computed: {
+      images() {
+        return this.carts.flatMap(cart => cart.Pictures)
+      }
+    },
+
+    mounted(){
+      axios.get(
+        'http://localhost:1337/carts'
+      )
+        .then(res => {
+          this.carts = res.data
+        })
+    }
   }
 </script>
 
 <style scoped>
-
+.gallery{
+  background-color: red;
+}
 </style>
